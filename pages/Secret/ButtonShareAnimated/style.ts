45e7bce36d58c314294e8c25a167c6a24b562e1f
Miguel Penha <miguel.penha1@gmail.com>
2022-11-06 23:01:34 -0300

    Ajeitando botão de compartilhar segredo

--- pages/Secret/ButtonShareAnimated/style.ts
+++ pages/Secret/ButtonShareAnimated/style.ts
@@ -4,13 +4,13 @@ import { RFPercentage } from 'react-native-responsive-fontsize'
 import { MaterialIcons } from '@expo/vector-icons'
 
 export const Container = styled(Animated.View)`
-    right: 0%;
-    bottom: -2%;
-    position: absolute;
+    margin-right: 5%;
+    margin-left: auto;
+    margin-bottom: 8%;
 `
 
 export const Button = styled.TouchableOpacity`
-    padding: 9%;
+    padding: 3%;
     align-self: center;
     border-radius: ${RFPercentage(4)}px;
     background-color: ${props => props.theme.primary};
--- pages/Secret/style.ts
+++ pages/Secret/style.ts
@@ -22,7 +22,7 @@ export const ContainerValue = styled.TouchableOpacity`
     padding: 5%;
     max-width: 90%;
     margin-top: 10%;
-    margin-bottom: 28%;
+    margin-bottom: 8%;
     align-self: center;
     border-radius: ${RFPercentage(2)}px;
     background-color: ${props => props.theme.backgroundColorSecondary};
