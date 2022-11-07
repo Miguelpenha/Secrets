d89120162f51029863648bb40e2989a8a94b2a51
Miguel Penha <miguel.penha1@gmail.com>
2022-11-07 07:07:35 -0300

    Update style.ts

--- pages/Settings/style.ts
+++ pages/Settings/style.ts
@@ -2,7 +2,7 @@ import styled from 'styled-components/native'
 import { RFPercentage } from 'react-native-responsive-fontsize'
 
 export const ContainerOptions = styled.ScrollView`
-    margin-top: 19%;
+    margin-top: 22%;
 `
 
 export const ContainerSwitch = styled.View`
