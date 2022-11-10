c7ea987d496c24066dc9f1be968160c95c2661a1
Miguel Penha <miguel.penha1@gmail.com>
2022-11-10 02:42:36 -0300

    Removendo MenuContainer

--- pages/Home/Secrets/Secret/ContainerMenu/style.ts
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components/native'
-import { Menu as MenuNotStyled } from 'react-native-material-menu'
-import { RFPercentage } from 'react-native-responsive-fontsize'
-
-export const Menu = styled(MenuNotStyled)`
-    border-radius: ${RFPercentage(2)}px;
-    background-color: ${props => props.theme.backgroundColorSecondary};
-`
--- pages/Home/Secrets/Secret/index.tsx
+++ pages/Home/Secrets/Secret/index.tsx
@@ -1,13 +1,11 @@
 import { ISecret } from '../../../../types'
-import { FC, useState } from 'react'
+import { FC } from 'react'
 import { useNavigation } from '@react-navigation/native'
 import useAnimations from './useAnimations'
 import { Container, Icon, Name, Next } from './style'
 import { RFPercentage } from 'react-native-responsive-fontsize'
 import limitText from '../../../../utils/limitText'
 import { Dimensions } from 'react-native'
-import { Menu, MenuItem } from 'react-native-material-menu'
-import ContainerMenu from './ContainerMenu'
 
 interface Iprops {
     secret: ISecret
@@ -16,25 +14,22 @@ interface Iprops {
 
 const Secret: FC<Iprops> = ({ secret, onVerify }) => {
     const navigation = useNavigation()
-    const [openMenu, setOpenMenu] = useState(false)
     const { animationPressed, animationPressedIcon, animationPressedName, animationPressedNext, events } = useAnimations()
     
     return (
-        <ContainerMenu openMenu={openMenu} setOpenMenu={setOpenMenu}>
-            <Container onLongPress={() => setOpenMenu(true)} style={animationPressed} {...events(() => {
-                if (secret.secure) {
-                    onVerify(secret.id)
-                } else {
-                    navigation.navigate('Secret', { id: secret.id })
-                }
-            })}>
-                <Icon size={RFPercentage(4)} name={!secret.hideIcon ? secret.icon : 'lock'} style={animationPressedIcon}/>
-                <Name editable={false} numberOfLines={1} secureTextEntry={secret.hideName} style={animationPressedName}>
-                    {(!secret.hideName) ? limitText(secret.name, Dimensions.get('window').scale*8) : '                   '}
-                </Name>
-                <Next name="arrow-forward-ios" size={RFPercentage(4)} style={animationPressedNext}/>
-            </Container>
-        </ContainerMenu>
+        <Container style={animationPressed} {...events(() => {
+            if (secret.secure) {
+                onVerify(secret.id)
+            } else {
+                navigation.navigate('Secret', { id: secret.id })
+            }
+        })}>
+            <Icon size={RFPercentage(4)} name={!secret.hideIcon ? secret.icon : 'lock'} style={animationPressedIcon}/>
+            <Name editable={false} numberOfLines={1} secureTextEntry={secret.hideName} style={animationPressedName}>
+                {(!secret.hideName) ? limitText(secret.name, Dimensions.get('window').scale*8) : '                   '}
+            </Name>
+            <Next name="arrow-forward-ios" size={RFPercentage(4)} style={animationPressedNext}/>
+        </Container>
     )
 }
 
